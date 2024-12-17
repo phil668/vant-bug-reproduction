ee94225ebc90b12cb8e2276c5aab1ff0e39867a9
Phil Liu <1308972436@qq.com>
2024-12-17 23:02:13 +0800

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <van-calendar :poppable="false" :show-confirm="false" :min-date="new Date(2025, 2, 1)"
+    :max-date="new Date(2025, 3, 31)" type="range" :default-date="[new Date(2025, 2, 15), new Date(2025, 3, 3)]" />
+</template>
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import { Calendar } from 'vant';
+import 'vant/lib/index.css';
+
+createApp(App).use(Calendar).mount('#app')
+
+
+
